6f5864e0ba7850b14338d8f68ec68b7a5f6a4447
smartoolhanif <clashofkinghanif44@gmail.com>
2025-11-27 11:50:37 +0600

    Upload Users.java

---
+++ app/src/main/java/com/sms/sopnopay/Users.java
@@ -0,0 +1,35 @@
+package com.sms.sopnopay;
+
+public class Users {
+
+
+
+    String title, body;
+
+
+    public Users() {
+
+
+    }
+
+    public Users(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
